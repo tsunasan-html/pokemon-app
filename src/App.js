2f592b1750e998f99975c52f044d9554ebad5594
tsunasan-html <>
2023-11-19 15:21:38 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import './App.css';
+import { getAllPokemon } from './utils/pokemon';
+
+function App() {
+  const initialURL =  "https://pokeapi.co/api/v2/pokemon";
+
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
+
+
+  return (
+    <div className="App"></div>
+  );
+}
+
+export default App;
